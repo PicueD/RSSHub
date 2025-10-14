bee8f2b250ac21b1ba2018ba26c5f8a3a95bf71e
PicueD <167000175+PicueD@users.noreply.github.com>
2025-10-14 17:38:30 +0800

    Update the `ofetch.ts` file to enable HTTP/2 support, aiming to improve network request performance.

--- lib/utils/ofetch.ts
+++ lib/utils/ofetch.ts
@@ -1,11 +1,17 @@
 import { createFetch } from 'ofetch';
+import { Agent as UndiciAgent, request as undiciRequest } from 'undici';
 import { config } from '@/config';
 import logger from '@/utils/logger';
 import { register } from 'node-network-devtools';
 
 config.enableRemoteDebugging && process.env.NODE_ENV === 'dev' && register();
-
+// 创建支持 HTTP/2 的 undici Agent
+const agent = new UndiciAgent({
+  keepAliveTimeout: 10,
+  allowH2: true, // ✅ 优先使用 HTTP/2
+});
 const rofetch = createFetch().create({
+    dispatcher: agent, // 使用自定义 undici Agent
     retryStatusCodes: [400, 408, 409, 425, 429, 500, 502, 503, 504],
     retry: config.requestRetry,
     retryDelay: 1000,
